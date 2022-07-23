6099a9fcf960a9f36c4facc0ed1adc84e8bb12ab
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-23 19:18:21 -0300

    Product list model data implemented

--- HVC.Store.UI/Controllers/ProductsController.cs
+++ HVC.Store.UI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HVC.Store.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,15 @@ using System.Web.Mvc;
 namespace HVC.Store.UI.Controllers {
     public class ProductsController:Controller {
         public ViewResult Index() {
-            return View();
+            var products = new List<Product>() {
+                new Product() { Id = 1, Name = "Picanha", Type = "Carnes", Quantity = 1, Price = 100.0M, DateEntry = DateTime.Parse("10/05/2015") },
+                new Product() { Id = 2, Name = "Linguiça", Type = "Carnes", Quantity = 2, Price = 30.0M, DateEntry = DateTime.Parse("18/08/2017") },
+                new Product() { Id = 3, Name = "Frango a Passarinho", Type = "Carnes", Quantity = 3, Price = 40.0M, DateEntry = DateTime.Parse("05/07/2010") },
+                new Product() { Id = 4, Name = "Carvão", Type = "Churrasco", Quantity = 1, Price = 25.0M, DateEntry = DateTime.Parse("25/01/2012") },
+                new Product() { Id = 5, Name = "Cerveja", Type = "Bebidas", Quantity = 30, Price = 4.0M, DateEntry = DateTime.Parse("30/06/2011") },
+                new Product() { Id = 5, Name = "Coca", Type = "Bebidas", Quantity = 5, Price = 10.0M, DateEntry = DateTime.Parse("07/03/2014") },
+            };
+            return View(products);
         }
     }
 }
---
+++ HVC.Store.UI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVC.Store.UI.Models {
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public short Quantity { get; set; }
+        public DateTime DateEntry { get; set; }
+    }
+}
