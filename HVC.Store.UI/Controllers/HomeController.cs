4dd234530b8686ba24fea35910bf4252954ea2f3
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-23 18:52:26 -0300

    Products list string form

---
+++ HVC.Store.UI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HVC.Store.UI.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            return View();
+        }
+        public ViewResult About() {
+            return View();
+        }
+    }
+}
---
+++ HVC.Store.UI/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HVC.Store.UI.Controllers {
+    public class ProductsController:Controller {
+        public ViewResult Index() {
+            return View();
+        }
+    }
+}
